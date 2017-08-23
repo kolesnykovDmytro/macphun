44a4189e18bd607e9a3122bd3d16f297589fbd8e
kolesnykovDmytro <kolesnykovdmytro@gmail.com>
2017-08-23 16:52:02 +0300

    Add files via upload

---
+++ app/send.php
@@ -0,0 +1,18 @@
+<?
+if((isset($_POST['email'])&&$_POST['email']!="")){
+        $to = 'kolesnykovdmytro@gmail.com';
+        $subject = 'Обратный звонок';
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['email'].'</p>                   
+                    </body>
+                </html>';
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "From: Отправитель <from@example.com>\r\n";
+        mail($to, $subject, $message, $headers);
+}
+?>
